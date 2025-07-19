026ac29becbf883d25dba6e2312d689ab664ac23
Eri <alonsoalonsl431432@gmail.com>
2025-07-19 13:43:00 -0300

    Portfolio11

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,9 +4,12 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <head>
+        <title>Martín Alonso</title>
+        <meta name="description" content="Bienvenido a mi portfolio" />
+        <link rel="icon" href="/icon.ico" />
+      </head>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
